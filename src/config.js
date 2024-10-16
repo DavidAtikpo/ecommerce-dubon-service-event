dc1f3c2c1346766955847448f01bea7828a00b43
DavidAtikpo <davidatikpo44@gmail.com>
2024-10-16 06:02:14 +0000

    categorie

--- src/config.js
+++ src/config.js
@@ -1,9 +1,3 @@
-// export const API_URL = 'http://192.168.1.69:5000';
 
-<<<<<<< HEAD
-export const API_URL = 'http://localhost:5000';
-// export const API_URL = 'https://ecommerce-dubon-service-event.vercel.app';
-=======
 // export const API_URL = 'http://localhost:5000';
-export const API_URL = 'https://ecommerce-dubon-service-event.vercel.app';
->>>>>>> 0257ab6bcf04ac1c7cda2ea754b9fc6b1f147769
+export const API_URL = 'https://ecommerce-dubon-service-event.vercel.app'

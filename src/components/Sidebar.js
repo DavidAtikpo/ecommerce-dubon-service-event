285fb3565837fadce6dfa9e9ce0a80821e3f9f6c
DavidAtikpo <davidatikpo44@gmail.com>
2024-10-19 18:10:30 +0000

    update backend

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -74,13 +74,13 @@ const Sidebar = ({ sidebarToggle, onMenuClick }) => {
           </ul>
         )}
 
-        <li 
+        {/* <li 
           className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2 cursor-pointer"
           onClick={() => handleMenuClick('blogs')}
         >
           <FaRegFileAlt className="inline-block w-6 h-6 mr-2 -mt-2" />
           Blogs
-        </li>
+        </li> */}
 
         <li 
           className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2 cursor-pointer"
